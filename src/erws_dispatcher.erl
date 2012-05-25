b338e332083ebb6c0672e1dacb0cf676f7115f33
Marcelo Gornstein <marcelog@gmail.com>
2012-05-25 14:40:28 -0300

    added some debugging

--- src/erws_dispatcher.erl
+++ src/erws_dispatcher.erl
@@ -9,23 +9,27 @@
     websocket_info/3, websocket_terminate/3
 ]).
 
-init({tcp, http}, _Req, _Opts) ->
+init({tcp, http}, Req, _Opts) ->
+    lager:debug("Request: ~p", [Req]),
     {upgrade, protocol, cowboy_http_websocket}.
 
-handle(_Req, State) ->
-    {ok, Req2} = cowboy_http_req:reply(200, [{'Content-Type', <<"text/html">>}]),
+handle(Req, State) ->
+    lager:debug("Request not expected: ~p", [Req]),
+    {ok, Req2} = cowboy_http_req:reply(404, [{'Content-Type', <<"text/html">>}]),
     {ok, Req2, State}.
 
 terminate(_Req, _State) ->
     ok.
 
 websocket_init(_Any, Req, []) ->
+    lager:debug("New client"),
     Req2 = cowboy_http_req:compact(Req),
     {ok, Req2, undefined, hibernate}.
 
 websocket_handle({text, Msg}, Req, State) ->
+    lager:debug("Got Data: ~p", [Msg]),
     {reply,
-        {text, << "responding to hi", Msg/binary >>}, Req, State, hibernate
+        {text, << "responding to ", Msg/binary >>}, Req, State, hibernate
     };
 
 websocket_handle(_Any, Req, State) ->
